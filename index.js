4a9bdfa231b06d157b6f67fca8ab251fbc5a4137
dineshyugi <123645040+dineshyugi@users.noreply.github.com>
2024-04-08 17:07:53 +0530

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Watching YouTube"];
+const statusMessages = ["Listening HISTORIC ROLEPLAY","Watching ACTIVITIES"];
 
 
 let currentIndex = 0;
